7a6fa3de9163968cbd9d71bd02a6b10ac3090104
David Gomez Muns <davidmuns@yahoo.es>
2021-11-15 20:46:26 +0100

    cambio puerto tomcat

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
@@ -1,9 +1,9 @@
 package com.businessassistantbcn.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1")
