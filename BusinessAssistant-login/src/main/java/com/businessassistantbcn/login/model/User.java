fe778823c81860a91dcf2b82c13989c4e97da588
Joel Cabatay <asci274@gmail.com>
2021-11-16 19:07:59 +0100

    User class created

---
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/model/User.java
@@ -0,0 +1,31 @@
+package com.businessassistantbcn.login.model;
+
+public class User {
+    private String user;
+    private String pwd;
+    private String token;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
