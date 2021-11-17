8092b7d2f3d00267f549a158c4d68a470aeb45a5
alopezviv <peopole@gmail.com>
2021-11-17 22:57:42 +0100

    SecurityConfigModificada

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.businessassistantbcn.login.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
@@ -14,8 +16,10 @@ import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterAfter(new JwtAuthenticationFilter(), 
 				UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests().mvcMatchers("/v1/api/login").permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
