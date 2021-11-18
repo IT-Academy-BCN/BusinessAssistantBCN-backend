56962c6f02329d5114b4b6ec1c61c133ec62ad3b
R-Torrent <rta@alumnes.upm.es>
2021-11-18 01:22:31 +0100

    lista de perfiles 'authorities' desactivada

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
@@ -1,8 +1,13 @@
-	package com.businessassistantbcn.login.security;
+package com.businessassistantbcn.login.security;
+
+/**
+ * El código comentado debe reincorporarse si los JWT de autorización empiezan a incluir
+ * entre sus claims una lista de perfiles con rótulo SecurityConstants.AUTHORITIES
+ */
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+//import java.util.List;
+//import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,8 +23,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     
@@ -34,8 +39,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	        	 SecurityContextHolder.clearContext();
 	        else {
 	        	Claims claims = validateToken(request);
+/*	        	
 	        	// Comprobación que el token contiene claims 'exp' y AUTHORITIES
-	        	if(claims.getExpiration() != null && claims.get(SecurityConstants.AUTHORITIES) != null)
+	        	if(claims.getExpiration() != null) && claims.get(SecurityConstants.AUTHORITIES) != null)
+*/
+	        	if(claims.getExpiration() != null)
 	        		setUpSpringAuthentication(claims);
 	        	else
 	        		SecurityContextHolder.clearContext();
@@ -62,13 +70,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	}
     
 	private void setUpSpringAuthentication(Claims claims) {
-		@SuppressWarnings("unchecked")
+/*		@SuppressWarnings("unchecked")
 		List<String> authorities = (List<String>)claims.get(SecurityConstants.AUTHORITIES);
 		
 		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
 				claims.getSubject(),
 				null,
 				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+*/
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				claims.getSubject(), null);
+		
 		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 	}
     
