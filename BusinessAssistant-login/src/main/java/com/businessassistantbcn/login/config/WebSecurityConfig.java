35a9cf13419d1bbb4c1e59741a8b86a994d12441
R-Torrent <rta@alumnes.upm.es>
2021-11-19 07:00:15 +0100

    Trabajo nocturno

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -2,14 +2,16 @@ package com.businessassistantbcn.login.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
-
+import com.businessassistantbcn.login.security.SecurityConstants;
 
 @Configuration
 @EnableWebSecurity
@@ -17,9 +19,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(new JwtAuthenticationFilter(), 
+		http.addFilterAfter(new JwtAuthenticationFilter(), 
 				UsernamePasswordAuthenticationFilter.class);
-		http.authorizeRequests().antMatchers(HttpMethod.POST,"/v1/api/login").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
+		http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 	}
 }
--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
@@ -1,27 +1,21 @@
 package com.businessassistantbcn.login.security;
 
-
-
-/*El código comentado debe reincorporarse si los JWT de autorización empiezan a incluir
+/*
+ * El código comentado debe reincorporarse si los JWT de autorización empiezan a incluir
  * entre sus claims una lista de perfiles con rótulo SecurityConstants.AUTHORITIES
  */
 
 import java.io.IOException;
-import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
+
 import java.util.List;
+//import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -41,41 +35,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     		FilterChain filterChain) throws IOException, ServletException {
     	try {
 	        String authorizationHeader = request.getHeader(SecurityConstants.HEADER_STRING);
-	        System.err.print(1);
-	        if(authorizationHeaderIsInvalid(authorizationHeader)){
-	        	 SecurityContextHolder.clearContext();
-	        	 System.err.print(2);	
-	         }else {
-	        	Claims claims = validateToken(request);
-	        	
+	        
+	        if(authorizationHeaderIsValid(authorizationHeader)){      
+	        	Claims claims = validateToken(request);        	
 /*	        	
 	        	// Comprobación que el token contiene claims 'exp' y AUTHORITIES
-	        	if(claims.getExpiration() != null) && claims.get(SecurityConstants.AUTHORITIES) != null)
+	        	if(claims.getExpiration() != null) && claims.get(SecurityConstants.AUTHORITIES) != null) {
 */
 	        	if(claims.getExpiration() != null) {
-	        		System.err.print("Claims not Null");
 	        		setUpSpringAuthentication(claims);
+	        		filterChain.doFilter(request, response);
+	        		return;
 	        	}
-	        	else
-	        		SecurityContextHolder.clearContext();
 	        }
-	        filterChain.doFilter(request, response);
+	        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     	} catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
-    		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     	}
     }
     
-    private boolean authorizationHeaderIsInvalid(String authorizationHeader) {
-    	if(authorizationHeader == null) {
-    		System.err.print("null header");
-    	}
-        return authorizationHeader == null || !authorizationHeader.startsWith(SecurityConstants.TOKEN_PREFIX);
+    private boolean authorizationHeaderIsValid(String authorizationHeader) {
+        return authorizationHeader != null && authorizationHeader.startsWith(SecurityConstants.TOKEN_PREFIX);
     }
     
     private Claims validateToken(HttpServletRequest request) {
 		String jwtToken = request.getHeader(SecurityConstants.HEADER_STRING).replace(SecurityConstants.TOKEN_PREFIX, "");
-		//System.err.print("Token " + jwtToken);
+		
 		return Jwts.parserBuilder()
 				.setSigningKey(SecurityConstants.SECRET.getBytes())
 				.build()
@@ -92,11 +77,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 				null,
 				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
 */
-		System.err.print(claims.getSubject());
-		List<GrantedAuthority> authorities = new ArrayList<>();
 		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-				claims.getSubject(), null,authorities);
-		System.err.print(authenticationToken);
+				claims.getSubject(), null, List.of());
 		
 		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 	}
