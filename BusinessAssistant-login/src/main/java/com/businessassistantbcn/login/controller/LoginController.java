b704b35fdf872f2b27ab21c186dc6b9c8f0e84d2
R-Torrent <rta@alumnes.upm.es>
2021-11-22 18:01:54 +0100

    YAML

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
@@ -1,8 +1,6 @@
 package com.businessassistantbcn.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
