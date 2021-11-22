8b0ef3b9836793afb0928e35613dffee733ad6b3
Joel Cabatay <asci274@gmail.com>
2021-11-22 11:24:13 +0100

    Disable login default

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/App.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/App.java
@@ -2,8 +2,10 @@ package com.businessassistantbcn.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication //disable to show login // defaultconfig
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) //disable to hide login 
 public class App {
 
     public static void main(String[] args) {
