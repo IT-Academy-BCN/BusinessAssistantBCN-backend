fccf21567d2f81298f198a272bfd9f460c2df072
R-Torrent <rta@alumnes.upm.es>
2021-11-23 15:08:24 +0100

    Mensaje incorporado a los 401

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/YAMLConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/YAMLConfig.java
@@ -14,6 +14,7 @@ public class YAMLConfig {
 	private String headerString;
 	private String tokenPrefix;
 	private String authorities; // lista de permisos (ROLES)
+	private String err;
 	
 	public String getSignUpUrl() {
 		return signUpUrl;
@@ -35,6 +36,10 @@ public class YAMLConfig {
 		return authorities;
 	}
 	
+	public String getErr() {
+		return err;
+	}
+	
 	public void setSignUpUrl(String signUpUrl) {
 		this.signUpUrl = signUpUrl;
 	}
@@ -53,6 +58,10 @@ public class YAMLConfig {
 	
 	public void setAuthorities(String authorities) {
 		this.authorities = authorities;
+	}
+	
+	public void setErr(String err) {
+		this.err = err;
 	}
 	
 }
--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.businessassistantbcn.login.config;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 import com.businessassistantbcn.login.YAMLConfig;
 import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
@@ -25,14 +30,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private YAMLConfig config;
 	
 	@Override
-	public void configure(HttpSecurity http) throws Exception{
+	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(jwtAuthenticationFilter, 
 				UsernamePasswordAuthenticationFilter.class);
 		http.authorizeRequests().antMatchers(HttpMethod.POST, config.getSignUpUrl()).permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
-		http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+		http.exceptionHandling().authenticationEntryPoint(new BasicAuthenticationEntryPoint() {
+			
+			@Override
+		    public void commence(HttpServletRequest request,
+		    		HttpServletResponse response, AuthenticationException authEx) throws IOException {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				response.getWriter().print(config.getErr());
+		    }
+			
+		});
 	}
 	
 }
