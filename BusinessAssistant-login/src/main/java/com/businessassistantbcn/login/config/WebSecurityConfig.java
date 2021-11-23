ca412794872588c9df34c2a376cc0ad81cb57edc
R-Torrent <rta@alumnes.upm.es>
2021-11-23 15:58:20 +0100

    Mensaje incorporado a los 401 (ver2)

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.businessassistantbcn.login.config;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 import com.businessassistantbcn.login.YAMLConfig;
 import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
@@ -32,8 +37,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				UsernamePasswordAuthenticationFilter.class);
 		http.authorizeRequests().antMatchers(HttpMethod.POST, config.getSignUpUrl()).permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
-
-		http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+		http.exceptionHandling().authenticationEntryPoint(new BasicAuthenticationEntryPoint() {
+			
+			@Override
+			public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				response.getWriter().print(config.getErr());
+			}
+			
+		});
 	}
 	
 }
