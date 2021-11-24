0dae68689c153ef4e4aa04b21a4c19532caf6eee
R-Torrent <rta@alumnes.upm.es>
2021-11-24 09:38:08 +0100

    Limpieza de archivos .gradle"

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private YAMLConfig config;
 	
 	@Override
-	public void configure(HttpSecurity http) throws Exception{
+	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(jwtAuthenticationFilter, 
