239de78ec729a367950626b8a1ff4209e3eb184c
AlbaGPadros <albapadros@gmail.com>
2021-12-15 10:42:50 +0100

    BABCN 20 2 (extensión correcta)

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
@@ -104,11 +104,11 @@ public class OpendataController {
     }
     
     //GET ?offset=0&limit=10
-    @GetMapping("/large-stablishments/activity?select=fields.activitat")
+    @GetMapping("/large-stablishments/activity")
     @ApiOperation("Get large stablishment activity SET 0 LIMIT 10")
     @ApiResponses({
             @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 404, message = "Not Found :("),
+            @ApiResponse(code = 404, message = "Not Found"),
     })
     public String largeEstablishmentsActivity()
     {
