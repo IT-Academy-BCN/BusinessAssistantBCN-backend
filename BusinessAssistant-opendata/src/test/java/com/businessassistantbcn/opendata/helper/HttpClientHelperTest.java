4f0893b5766596920a42fee77f620f730118b976
David Gomez Muns <davidmuns@yahoo.es>
2021-12-15 21:31:03 +0100

    Update HttpClientHelperTest.java
    testing getStringRoot method

--- BusinessAssistant-opendata/src/test/java/com/businessassistantbcn/opendata/helper/HttpClientHelperTest.java
+++ BusinessAssistant-opendata/src/test/java/com/businessassistantbcn/opendata/helper/HttpClientHelperTest.java
@@ -1,23 +1,32 @@
 package com.businessassistantbcn.opendata.helper;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.jupiter.api.Test;
+
+import com.businessassistantbcn.opendata.config.PropertiesConfig;
+
 public class HttpClientHelperTest {
+	
+	HttpClientHelper httpClientHelper = new HttpClientHelper(new PropertiesConfig());
 
-    @Test
-    public void getStringRootTest(){
+	@Test
+	public void getStringRootTest() throws MalformedURLException {
+		String url = "https://jsonplaceholder.typicode.com/todos/1";
+		String expected = "{\"userId\": 1,\"id\": 1, \"title\": \"delectus aut autem\",\"completed\": false\"}";
+		String result = httpClientHelper.getStringRoot(new URL(url));
+		assertEquals(expected, result, "The 'getStringRoot' method should return a stringJson");
+	}
 
-    }
+	@Test
+	public void getJsonRootTest() {
+	}
 
-    @Test
-    public void getJsonRootTest() {
-    }
+	@Test
+	public void getObjectRootTest() {
 
-    @Test
-    public void getObjectRootTest (){
-
-    }
+	}
 }
