b5d5aec3fdf296081dba2bfca9af7ea90596e8d0
Cristo <cristobal_richart@protonmail.com>
2021-12-17 10:16:34 +0100

    Add methods in class JsonHelper (package: opendata/helper) BABCN-32-02
    Cleaning code

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/helper/JsonHelper.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/helper/JsonHelper.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import ch.qos.logback.classic.Logger;
 import net.minidev.json.writer.ArraysMapper.GenericMapper;
@@ -14,25 +16,43 @@ import net.minidev.json.writer.ArraysMapper.GenericMapper;
 @Component
 public class JsonHelper {
 
-	private static ObjectMapper mapper = new ObjectMapper();
-		
-    public <T> List<T> deserializeToList(String json){
-        return null;
-    }
+	private static ObjectMapper mapper;
 
-    public JsonNode deserializeToJsonNode(String json){
-        return null;
-    }
+	static {
+		mapper = new ObjectMapper();
+	}
 
-    public String serialize(JsonNode node) {
-    	String jsonString = null;
-    		try {
-				jsonString = mapper.writeValueAsString(node);
-			} catch (JsonProcessingException e) {
-				System.out.println("El error es " + e.getMessage());
-				e.printStackTrace();
-			}		
-    	return jsonString;
-    }
- 
+	public static <T> List<T> deserializeToList(String json){
+		List<T> list = null;
+		try {
+			list = (List<T>) mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			System.out.println("No se puede convertir de String a una Lista Generica porque: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public static JsonNode deserializeToJsonNode(String json){
+		JsonNode jsonNode = null;
+		try {
+			jsonNode = mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			System.out.println("No se puede convertir de String to JsonNode porque: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return jsonNode;
+	}
+
+	public static String serialize(JsonNode node) {
+		String jsonString = null;
+		try {
+			jsonString = mapper.writeValueAsString(node);
+		} catch (JsonProcessingException e) {
+			System.out.println("No se puede convertir de JsonNode to String porque: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return jsonString;
+	}
+
 }
