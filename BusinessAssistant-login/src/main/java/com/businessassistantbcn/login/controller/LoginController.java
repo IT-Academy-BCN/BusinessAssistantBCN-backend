86adcefa287715c5692e8c306651955b76bde84c
R-Torrent <rta@alumnes.upm.es>
2021-12-21 12:26:16 +0100

    logger para servicio \'login\'

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.businessassistantbcn.login.controller;
 
+import com.businessassistantbcn.login.config.LoggerConfig;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ public class LoginController {
     
     @GetMapping("/test")
     public String test() {
+    	LoggerConfig.log.info("** Saludos desde el logger **");
         return "Hello from BusinessAssistant Barcelona!!!";
     }
     
