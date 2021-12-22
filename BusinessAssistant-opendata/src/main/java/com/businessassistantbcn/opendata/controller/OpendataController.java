95ce09d8ac42282d6e9756afcfad3e3bf99d897c
Jonatan Vicente <it@jonatanvicente.com>
2021-12-22 12:47:32 +0100

    Refactoring a bit...

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
@@ -95,18 +95,20 @@ public class OpendataController {
     @ApiOperation("Get big malls SET 0 LIMIT 10")
     @ApiResponses({
             @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Offset or Limit cannot be null"),
             @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 503, message = "Service Unavailable"),
     })
-    public <T> Mono<T> bigMalls(@RequestParam(required = false) String offset, @RequestParam(required = false)  String limit)
-    {
+    public <T> Mono<T> bigMalls(
+            @ApiParam(value = "Offset", name= "Offset")
+            @RequestParam(required = false) String offset,
+            @ApiParam(value = "Limit", name= "Limit")
+            @RequestParam(required = false)  String limit){
 
-        if(offset == null){
-            offset= "0";
-        }
-        if(limit == null){
-            limit = "-1";
-        }
-        if(offset.equals("") || limit.equals("") || offset.equals("null") || limit.equals("null")  ){
+        offset = offset == null ? "0": offset;
+        limit = limit == null ? "-1": limit;
+
+        if(offset.compareTo("")==0 || limit.compareTo("")==0 || offset.compareTo("null")==0 || limit.compareTo("null")==0){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Offset or Limit cannot be null");
         }
         try{
@@ -114,8 +116,6 @@ public class OpendataController {
         }catch (Exception mue){
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Resource not found", mue);
         }
-
-
     }
 
     //GET ?offset=0&limit=10
