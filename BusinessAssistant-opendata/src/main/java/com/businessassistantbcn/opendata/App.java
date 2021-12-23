5f10e232988af3991433dbcc5cd8b631fe19f4c3
Jonatan Vicente <it@jonatanvicente.com>
2021-12-23 14:08:50 +0100

    Avoiding conflicts

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
@@ -2,13 +2,13 @@ package com.businessassistantbcn.opendata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class App {
 
     public static void main(String[] args) {
