d3a23ffff3003efc0bab24b2643106cf38da50e6
Joel Cabatay <ascii274>
2022-01-10 17:45:53 +0100

    Log.error added

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/EconomicActivitiesCensusService.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/EconomicActivitiesCensusService.java
@@ -1,11 +1,13 @@
 package com.businessassistantbcn.opendata.service;
 
-import com.businessassistantbcn.opendata.config.LoggerConfig;
+
 import com.businessassistantbcn.opendata.config.PropertiesConfig;
 import com.businessassistantbcn.opendata.dto.GenericResultDto;
 import com.businessassistantbcn.opendata.dto.economicactivitiescensus.EconomicActivitiesCensusDto;
 import com.businessassistantbcn.opendata.helper.HttpClientHelper;
 import com.businessassistantbcn.opendata.helper.JsonHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import java.net.URL;
 @Service
 public class EconomicActivitiesCensusService {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonHelper.class);
     @Autowired
     PropertiesConfig config;
     @Autowired
@@ -31,17 +34,17 @@ public class EconomicActivitiesCensusService {
                     new URL(config.getDs_economicactivitiescensus()), EconomicActivitiesCensusDto[].class);
                 return response.flatMap( dto -> {
                     try{
-                    EconomicActivitiesCensusDto[] filteredDto = JsonHelper.filterDto(dto,offset,limit);
-                    genericResultDto.setLimit(limit);
-                    genericResultDto.setOffset(offset);
-                    genericResultDto.setResults(filteredDto);
-                    genericResultDto.setCount(dto.length);
-                    return Mono.just(genericResultDto);
-                } catch (Exception e){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+                        EconomicActivitiesCensusDto[] filteredDto = JsonHelper.filterDto(dto,offset,limit);
+                        genericResultDto.setLimit(limit);
+                        genericResultDto.setOffset(offset);
+                        genericResultDto.setResults(filteredDto);
+                        genericResultDto.setCount(dto.length);
+                        return Mono.just(genericResultDto);
+                     } catch (Exception e){
+                        log.error("Error con el filtrado de EconomicAtivitiesCensus" +  e.getMessage());
+                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
                 }
             });
-
         } catch (MalformedURLException mue) {
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Resource not found", mue);
         }
