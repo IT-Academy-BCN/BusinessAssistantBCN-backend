f71600a99ddf462559a9dc6ccffe589c8bcdb48c
Joel Cabatay <ascii274>
2022-01-10 10:45:39 +0100

    Try catch surrounded

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/EconomicActivitiesCensusService.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/EconomicActivitiesCensusService.java
@@ -5,7 +5,9 @@ import com.businessassistantbcn.opendata.dto.GenericResultDto;
 import com.businessassistantbcn.opendata.dto.economicactivitiescensus.EconomicActivitiesCensusDto;
 import com.businessassistantbcn.opendata.helper.HttpClientHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.net.MalformedURLException;
@@ -21,15 +23,19 @@ public class EconomicActivitiesCensusService {
     @Autowired
     GenericResultDto<EconomicActivitiesCensusDto> genericResultDto;
 
-    public Mono<GenericResultDto<EconomicActivitiesCensusDto>> getAllData()throws MalformedURLException {
-        Mono<EconomicActivitiesCensusDto[]> response = helper.getRequestData(
-                new URL(config.getDs_economicactivitiescensus()), EconomicActivitiesCensusDto[].class);
-        return response.flatMap( dto -> {
-            genericResultDto.setResults(dto);
-            genericResultDto.setCount(dto.length);
-            return Mono.just(genericResultDto);
-        });
+    public Mono<GenericResultDto<EconomicActivitiesCensusDto>> getAllData() {
+        Mono<EconomicActivitiesCensusDto[]> response = null;
+        try {
+            response = helper.getRequestData(
+                    new URL(config.getDs_economicactivitiescensus()), EconomicActivitiesCensusDto[].class);
+            return response.flatMap( dto -> {
+                genericResultDto.setResults(dto);
+                genericResultDto.setCount(dto.length);
+                return Mono.just(genericResultDto);
+            });
+
+        } catch (MalformedURLException e) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Resource not found", e);
+        }
     }
-
-
 }
