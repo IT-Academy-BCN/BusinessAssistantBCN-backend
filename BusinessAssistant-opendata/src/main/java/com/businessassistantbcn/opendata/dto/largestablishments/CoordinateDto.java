1594f3783d5c6192ea35b8527c30b76967a65547
Joel Cabatay <ascii274>
2022-01-13 14:25:54 +0100

    CoordinateDto class created

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/largestablishments/CoordinateDto.java
@@ -0,0 +1,11 @@
+package com.businessassistantbcn.opendata.dto.largestablishments;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CoordinateDto {
+
+    @JsonProperty("x")
+    private float x;
+    @JsonProperty("y")
+    private float y;
+}
