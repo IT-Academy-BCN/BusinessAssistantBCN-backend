fab90a7535f46bb4ec8b4c49fb90052b07035876
R-Torrent <rta@alumnes.upm.es>
2022-01-13 04:07:16 +0100

    OpendataControllerTest v0.1

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/CoordinateDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/CoordinateDto.java
@@ -3,8 +3,17 @@ package com.businessassistantbcn.opendata.dto.bigmalls;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CoordinateDto {
+	
     @JsonProperty("x")
-    float x;
+    public double x;
     @JsonProperty("y")
-    float y;
+    public double y;
+    
+    public CoordinateDto() {}
+    
+    public CoordinateDto(double x, double y) {
+    	this.x = x;
+    	this.y = y;
+    }
+    
 }
