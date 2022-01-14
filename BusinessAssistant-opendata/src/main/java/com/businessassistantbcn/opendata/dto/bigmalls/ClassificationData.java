df1437ff930dea4ff8880a2123d59322f7310ad7
Joel Cabatay <ascii274@gmail.com>
2022-01-15 00:22:31 +0100

    ClasificationData class updated for refactorization

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/ClassificationData.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/ClassificationData.java
@@ -1,6 +1,23 @@
 package com.businessassistantbcn.opendata.dto.bigmalls;
 
-public class ClassificationDataDto {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.stereotype.Component;
+
+//TODO this object is the same inside package largeStablishments, it can be share.
+
+@Component
+@JsonIgnoreProperties({ "full_path","dependency_group","parent_id","tree_id","asia_id","core_type","level" })
+public class ClassificationData {
+
+    // Factorisize properties
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
+    private String name;
+
+    // Default properties
+    /*
     private int id;
     private String name;
     private String full_path;
@@ -10,4 +27,5 @@ public class ClassificationDataDto {
     private String asia_id;
     private String core_type;
     private int level;
+    */
 }
