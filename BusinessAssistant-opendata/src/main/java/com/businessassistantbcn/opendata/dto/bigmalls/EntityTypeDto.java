fa08627f56152a046db781426b542486bc1c34ec
Joel Cabatay <ascii274@gmail.com>
2022-01-15 00:55:20 +0100

    Deleting class not needed in big-malls

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/EntityTypeDto.java
+++
@@ -1,6 +0,0 @@
-package com.businessassistantbcn.opendata.dto.bigmalls;
-
-public class EntityTypeDto {
-    private int id;
-    private String name;
-}
--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bigmalls/TimetableDto.java
+++
@@ -1,6 +0,0 @@
-package com.businessassistantbcn.opendata.dto.bigmalls;
-
-public class TimetableDto {
-    int id;
-    String html;
-}
