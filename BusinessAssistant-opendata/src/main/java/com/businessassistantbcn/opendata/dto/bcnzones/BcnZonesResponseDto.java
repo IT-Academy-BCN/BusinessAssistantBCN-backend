771df63f3ef27f2fa4974ec765f88ce916eed628
R-Torrent <rta@alumnes.upm.es>
2022-01-19 17:42:46 +0100

    Lombok et al, Mk III

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bcnzones/BcnZonesResponseDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bcnzones/BcnZonesResponseDto.java
@@ -3,11 +3,9 @@ package com.businessassistantbcn.opendata.dto.bcnzones;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class BcnZonesResponseDto {
 	
