19ad45ca9e486b74ff66404f14b67538c81d8b02
Jonatan Vicente <it@jonatanvicente.com>
2022-01-19 12:35:03 +0100

    Refactoring output BCN zones

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bcnzones/BcnZonesDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/bcnzones/BcnZonesDto.java
@@ -1,18 +1,39 @@
 package com.businessassistantbcn.opendata.dto.bcnzones;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import org.springframework.stereotype.Component;
 
 
 public class BcnZonesDto {
-    @JsonProperty("idZone")
-    private int id;
-    @JsonProperty("zoneName")
-    private String name;
+    /*@JsonProperty("id")*/
+    private int idZone;
+   /* @JsonProperty("name")*/
+    private String zoneName;
 
     public BcnZonesDto(int idZone, String zoneName) {
+        this.idZone = idZone;
+        this.zoneName = zoneName;
+    }
 
-        this.id = idZone;
-        this.name = zoneName;
+    @JsonGetter("idZone")
+    public int getId() {
+        return idZone;
+    }
+
+    @JsonSetter("id")
+    public void setId(int id) {
+        this.idZone = id;
+    }
+
+    @JsonGetter("zoneName")
+    public String getName() {
+        return zoneName;
+    }
+
+    @JsonSetter("name")
+    public void setName(String name) {
+        this.zoneName = name;
     }
 }
