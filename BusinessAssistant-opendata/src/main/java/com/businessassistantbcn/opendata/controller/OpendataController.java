8d7a9478811692af6e6d1ded3ee67dd1207ecc82
Mustafa <aharrat@hotmail.com>
2022-02-01 12:44:16 +0100

    incorporated endpoint municipal-markets/district/{districtId}

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/OpendataController.java
@@ -179,7 +179,33 @@ public class OpendataController {
         }
     }
     
-	//GET ?offset=0&limit=10
+    //GET ?offset=0&limit=10
+    @GetMapping("/municipal-markets/district/{district_id}")
+    @ApiOperation("Get municipal markets SET 0 LIMIT 10")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 400, message = "Offset or Limit cannot be null"),
+        @ApiResponse(code = 404, message = "Not Found"),
+        @ApiResponse(code = 503, message = "Service Unavailable"),
+    })
+    public Mono<?>municipalMarketsByDisctrict(
+    		@ApiParam(value = "Offset", name= "Offset")
+            @RequestParam(required = false) String offset,
+            @ApiParam(value = "Limit", name= "Limit")
+            @RequestParam(required = false)  String limit,
+            @PathVariable("district") int district_id){     //es integer por que en BusinessAssistantBCN-frontend/src/app/models/common/zone.model.ts es numérico
+    	  offset = offset == null ? "0": offset;
+          limit = limit == null ? "-1": limit;
+
+          try{
+        	  return municipalMarketsService.getPageByDistrict(Integer.parseInt(offset), Integer.parseInt(limit), district_id);
+            //falta generar el servicio en MunicipalMarketsServiceget:   PageByDistrict
+          }catch (Exception mue){
+              throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Resource not found", mue);
+          }
+    	
+    }
+    //GET ?offset=0&limit=10
 	@GetMapping("/market-fairs")
 	@ApiOperation("Get market fairs SET 0 LIMIT 10")
 	@ApiResponses({
