e28a4df84d1b70e324f093be7622bf58c8d59983
David Gomez Muns <davidmuns@yahoo.es>
2022-02-02 17:36:06 +0100

    Create CoordinateDto.java
    CoordinateDto class added to com.businessassistantbcn.opendata.dto.commercialgalleries

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/CoordinateDto.java
@@ -0,0 +1,19 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoordinateDto {
+	
+	@JsonProperty("x")
+	private double x;
+	@JsonProperty("y")
+	private double y;
+	
+}
