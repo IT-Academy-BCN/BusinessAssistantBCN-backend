352c6c5612d8b093e974938db747ed8a218d7430
David Gomez Muns <davidmuns@yahoo.es>
2022-02-02 17:27:29 +0100

    Create ClassificationDataDto.java
    ClassificationDataDto class added to com.businessassistantbcn.opendata.dto.commercialgalleries

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/ClassificationDataDto.java
@@ -0,0 +1,19 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ClassificationDataDto {
+	
+    private int id;
+    private String name;
+    private String full_path;
+    private int dependency_group;
+    private int parent_id;
+    private int tree_id;
+    private String asia_id;
+    private String core_type;
+    private int level;
+    
+}
