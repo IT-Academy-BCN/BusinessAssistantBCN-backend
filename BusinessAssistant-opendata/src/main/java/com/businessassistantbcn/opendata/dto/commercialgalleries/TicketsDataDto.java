1084a2ad82271481e0dfc08215979dad28cbfd14
David Gomez Muns <davidmuns@yahoo.es>
2022-02-04 20:06:41 +0100

    Create TicketsDataDto.java

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/TicketsDataDto.java
@@ -0,0 +1,11 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component("CommercialGalleriesTicketsDataDto")
+@Getter
+@Setter
+public class TicketsDataDto {
+}
