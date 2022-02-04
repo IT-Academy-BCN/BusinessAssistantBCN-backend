40b40789458bcbc689c9b5d32e842d76f4def74e
David Gomez Muns <davidmuns@yahoo.es>
2022-02-04 20:08:22 +0100

    Create EntityPrefixDto.java

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/EntityPrefixDto.java
@@ -0,0 +1,11 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component("CommercialGalleriesEntityPrefixDto")
+@Getter
+@Setter
+public class EntityPrefixDto {
+}
