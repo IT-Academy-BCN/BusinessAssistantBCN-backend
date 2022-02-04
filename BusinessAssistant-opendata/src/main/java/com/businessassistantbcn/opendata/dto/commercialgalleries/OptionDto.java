7f571bc4b76635b4bf4cc64b75e915a73e5e33f5
David Gomez Muns <davidmuns@yahoo.es>
2022-02-04 20:07:28 +0100

    Create OptionDto.java

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/OptionDto.java
@@ -0,0 +1,11 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component("CommercialGalleriesOptionDto")
+@Getter
+@Setter
+public class OptionDto {
+}
