0579df1cde0ade8062e6597f21b6f6776956b6cd
David Gomez Muns <davidmuns@yahoo.es>
2022-02-04 20:07:38 +0100

    Create ImageDataDto.java

---
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/commercialgalleries/ImageDataDto.java
@@ -0,0 +1,26 @@
+package com.businessassistantbcn.opendata.dto.commercialgalleries;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component("CommercialGalleriesImageDataDto")
+@Getter @Setter
+@JsonIgnoreProperties({"cuts","alt"})
+public class ImageDataDto {
+
+    private Long id;
+    private String name;
+    private String image;
+    private String title;
+    private String alt;//
+    private String author;
+    private String source;//
+    private String type_license;
+    private String description;
+    private String image_thumb;
+    private String image_optimized;
+//    private List<Cut> cuts;
+}
