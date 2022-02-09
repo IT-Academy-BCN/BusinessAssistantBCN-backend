076a72a5157760930267f981006c6f36717dea11
Isabel Domenech De Mena <isabel.160609@hotmail.com>
2022-02-09 20:24:55 +0100

    creado user y service para implementar el id del usuario

---
+++ BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/model/DatabaseSequence.java
@@ -0,0 +1,35 @@
+package com.businessassistantbcn.usermanagement.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+
+@Document(collection = "database_sequences")
+@Component
+public class DatabaseSequence {
+
+	//esta clase sirve para que haya un id autoincremental numerico para evitar el tipo de
+	//id que da mongo que suele dar comflicto
+    @Id
+    private String id;
+
+    private long seq;
+
+    public DatabaseSequence() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getSeq() {
+        return seq;
+    }
+
+    public void setSeq(long seq) {
+        this.seq = seq;
+    }
+}
