01a1c28ba1b020434c621868c3eb755504f0cd39
Vanet <vanraf2004@yahoo.es>
2022-02-21 10:40:50 +0100

    nombre añadido

---
+++ BusinessAssistant-mydata/bin/main/scripts-mysql/schema.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS businessassistantbcndb;
+CREATE DATABASE IF NOT EXISTS businessassistantbcndb;
+use businessassistantbcndb;
+
+/*
+Tabla Users:
+
+UUID
+email (varchar50)
+password (varchar50)
+creation_date (date)
+active (boolean)
+
+ */
---
+++ BusinessAssistant-usermanagement/bin/main/scripts-mongodb/create-database.js
@@ -0,0 +1,8 @@
+//show databases
+//show dbs;
+// create database
+use babcn-users;
+// create collection
+db.createCollection("users");
+// to read
+db.users.find({}).pretty();
