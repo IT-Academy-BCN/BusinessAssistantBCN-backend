417466f6a0a159e33e3c652cf3133d0a45f53f2b
Isabel Domenech De Mena <isabel.160609@hotmail.com>
2022-02-22 09:46:27 +0100

    add service de sequence

---
+++ BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/sequenceGenerator/service/SequenceGeneratorService.java
@@ -0,0 +1,37 @@
+package com.businessassistantbcn.usermanagement.sequenceGenerator.service;
+
+import static org.springframework.data.mongodb.core.query.Query.query;
+import java.util.Objects;
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import com.businessassistantbcn.usermanagement.model.DatabaseSequence;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+@Service
+public class SequenceGeneratorService {
+//este service es para generar el id incremetal del user uuid
+
+	@Autowired
+    private  MongoOperations mongoOperations;
+
+    @Autowired
+    public SequenceGeneratorService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public  long generateSequence(String seqName) {
+
+        DatabaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+                new Update().inc("seq",1), options().returnNew(true).upsert(true),
+                DatabaseSequence.class);
+        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+
+    }
+
+
+	
+}
