56824d60a708cd638d6029b83178f7992915ea62
Gustavo Martínez <gusmbox@gmail.com>
2022-02-23 11:29:50 +0100

    Added dependency for JPA. Cleaned build.gradle.

--- BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/controller/MydataController.java
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/controller/MydataController.java
@@ -1,16 +1,19 @@
 package com.businessassistantbcn.mydata.controller;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 
 @RestController
 @RequestMapping("/businessassistantbcn/api/v1/mydata")
 public class MydataController {
 
     @GetMapping(value="/test")
+    
+    // Swagger
     @ApiOperation("Get test")
     @ApiResponse(code = 200, message = "OK")
     public String test() {
