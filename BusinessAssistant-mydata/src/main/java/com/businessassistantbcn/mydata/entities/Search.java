e29d50ab0bd85c388d2592b7a14ac24060a1df86
Gustavo Martínez <gusmbox@gmail.com>
2022-02-23 22:03:28 +0100

    Added Search entity, Added Converter for MySQL to save JsonNode

---
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/entities/Search.java
@@ -0,0 +1,40 @@
+package com.businessassistantbcn.mydata.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.businessassistantbcn.mydata.helper.JsonNodeConverter;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "my_searches")
+public class Search {
+	@Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "VARCHAR(36)")
+	private String search_uuid;
+	private Date search_date;
+	private String search_name;
+	private String search_detail;
+	@Convert(converter = JsonNodeConverter.class)
+	private JsonNode search_result;
+	private String user_uuid;
+}
