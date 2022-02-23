c8135e671f398e5668929447c50025eef883469e
Jonatan Vicente <it@jonatanvicente.com>
2022-02-23 12:07:35 +0100

    Fix bug unrecognized properties Jackson
    Added Cache config

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/config/WebMVCConfig.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/config/WebMVCConfig.java
@@ -1,5 +1,9 @@
 package com.businessassistantbcn.opendata.config;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,9 +13,15 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @ComponentScan(basePackages = {"com.businessassistantbcn.opendata"})
 @Configuration
 @EnableWebMvc
+@EnableCaching
 public class WebMVCConfig {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("addresses");
     }
 }
 
