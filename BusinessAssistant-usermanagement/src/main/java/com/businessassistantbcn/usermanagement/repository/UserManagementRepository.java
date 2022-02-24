fd170784fb48f5220573edfb51cc99fe0c0002b7
Isabel Domenech De Mena <isabel.160609@hotmail.com>
2022-02-24 10:51:32 +0100

    add repository and change uuiid to long

---
+++ BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/repository/UserManagementRepository.java
@@ -0,0 +1,14 @@
+package com.businessassistantbcn.usermanagement.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.businessassistantbcn.usermanagement.dto.User;
+
+@Component
+@Repository
+public interface UserManagementRepository extends MongoRepository<User,Long>{
+
+}
+ 
