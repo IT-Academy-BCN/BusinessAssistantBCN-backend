02a452d65e9da16b3a438366608647335b4d17f2
Laura <laurabf6@gmail.com>
2022-02-25 12:56:12 +0100

    MySearchesRepository created

--- BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/repository/MySearchesRepository.java
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/repository/MySearchesRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.businessassistantbcn.mydata.entities.Search;
+
 @Repository
 @Transactional
 public interface MySearchesRepository extends JpaRepository<Search, String>{
 	
-	public boolean existsBySearch_uuid(String search_uuid);
-	public boolean existsByUser_uuid(String user_uuid);
+	public boolean existsBySearchUuid(String searchUuid);
+	public boolean existsByUserUuid(String userUuid);
 	
-	public List<Search> findBySearch_uuid(String search_uuid);
-	public List<Search> findByUser_uuid(String user_uuid);
+	public List<Object> findBySearchUuid(String searchUuid);
+	public List<Object> findByUserUuid(String userUuid);
 }
