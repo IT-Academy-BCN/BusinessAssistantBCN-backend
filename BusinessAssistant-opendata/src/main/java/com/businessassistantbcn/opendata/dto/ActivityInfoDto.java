9fe492aa3cee564ef7e71c972423ea1ddcec0b24
Isabel Domenech De Mena <isabel.160609@hotmail.com>
2022-02-27 20:37:34 +0100

    minim code circuit breakers

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/ActivityInfoDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/ActivityInfoDto.java
@@ -22,4 +22,6 @@ public class ActivityInfoDto
         this.activityName = activityName;
     }
 
+	
+
 }
--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/externaldata/CommercialGalleriesService.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/service/externaldata/CommercialGalleriesService.java
@@ -2,11 +2,15 @@ package com.businessassistantbcn.opendata.service.externaldata;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.businessassistantbcn.opendata.dto.ActivityInfoDto;
 import com.businessassistantbcn.opendata.dto.GenericResultDto;
-import com.businessassistantbcn.opendata.dto.bigmalls.BigMallsDto;
 import com.businessassistantbcn.opendata.dto.commercialgalleries.CommercialGalleriesDto;
-import com.businessassistantbcn.opendata.dto.marketfairs.MarketFairsDto;
 import com.businessassistantbcn.opendata.proxy.HttpProxy;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
@@ -34,6 +38,9 @@ public class CommercialGalleriesService {
 
 	@Autowired
 	private GenericResultDto<CommercialGalleriesDto> genericResultDto;
+	
+	@Autowired
+	private GenericResultDto<ActivityInfoDto> genericActivityResultDto;
 
 	@CircuitBreaker(name = "circuitBreaker", fallbackMethod = "getPageDefault")
 	public Mono<GenericResultDto<CommercialGalleriesDto>> getPage(int offset, int limit) throws MalformedURLException {
@@ -54,5 +61,50 @@ public class CommercialGalleriesService {
 		return Mono.just(genericResultDto);
 
 	}
+	
+	@CircuitBreaker(name = "circuitBreaker", fallbackMethod = "getPageDefaultActivity")
+	public Mono<GenericResultDto<ActivityInfoDto>> getCommercialGalleriesByActivity(int offset, int limit) throws MalformedURLException
+	{
+	return httpProxy.getRequestData(new URL(config.getDs_commercialgalleries()), CommercialGalleriesDto[].class)
+			.flatMap(comercialgalleriesDto -> {
+				List<ActivityInfoDto> listactivityInfoDto = new ArrayList<>();
+				listactivityInfoDto = io.vavr.collection.List.ofAll(
+					Arrays.stream(comercialgalleriesDto)
+						.flatMap(comercialGalleryDto -> comercialGalleryDto.getClassifications_data().stream())
+						.filter(classificationsDataDto ->
+							(classificationsDataDto.getFull_path() == null) ||
+								(
+									(!classificationsDataDto.getFull_path().toUpperCase().contains("MARQUES")) &&
+									(!classificationsDataDto.getFull_path().toUpperCase().contains("GESTIÓ BI")) &&
+									(!classificationsDataDto.getFull_path().toUpperCase().contains("ÚS INTERN"))
+								)
+						)
+						.map(classificationsDataDto -> {
+							return new ActivityInfoDto(
+									(long)classificationsDataDto.getId(),
+									((classificationsDataDto.getName() == null) ? "" : classificationsDataDto.getName())
+							);
+						})
+						.sorted(Comparator.comparing(ActivityInfoDto::getActivityName))
+						.collect(Collectors.toList()))
+				.distinctBy((s1, s2) -> s1.getActivityName().compareToIgnoreCase(s2.getActivityName()))
+				.toJavaList();
+
+				ActivityInfoDto[] activityInfoDto =
+					listactivityInfoDto.toArray(new ActivityInfoDto[listactivityInfoDto.size()]);
+
+				ActivityInfoDto[] pagedDto = JsonHelper.filterDto(activityInfoDto, offset, limit);
+				genericActivityResultDto.setInfo(offset, limit, activityInfoDto.length, pagedDto);
+				return Mono.just(genericActivityResultDto);
+			});
+	}
+	
+	private Mono<GenericResultDto<ActivityInfoDto>> getPageDefaultActivity(){
+		genericActivityResultDto.setLimit(0);
+		genericActivityResultDto.setOffset(0);
+		genericActivityResultDto.setResults(new ActivityInfoDto[0]);
+		genericActivityResultDto.setCount(0);
+		return Mono.just(genericActivityResultDto);
+	}		
 
 }
