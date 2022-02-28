faa58ff21c41744891044ce10363859e52b79797
David Gomez Muns <davidmuns@yahoo.es>
2022-02-28 18:08:48 +0100

    Update ClassificationDataDto.java

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/largeestablishments/ClassificationDataDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/largeestablishments/ClassificationDataDto.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -15,20 +14,27 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @AllArgsConstructor
 @Component("LargeEstablishmentsClassificationDataDto")
-@JsonIgnoreProperties({ "full_path","dependency_group","parent_id","tree_id","asia_id","core_type","level" })
+@JsonIgnoreProperties({"dependency_group","parent_id","tree_id","asia_id","core_type","level" })
 
 public class ClassificationDataDto {
 
     private Long id;
     private String name;
-
+    private String fullPath;
+    
     @JsonGetter("id")
     public Long getId() {
         return id;
     }
-
+	
     @JsonGetter("name")
     public String getName() {
         return name;
     }
+
+    @JsonGetter("full_path")
+    public String getFullPath() {
+        return fullPath;
+    }
+    
 }
