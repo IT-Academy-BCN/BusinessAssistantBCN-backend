9b818bdce1767fd8b9880a94aa02ffb5a4b7a8c3
yalipct <aliuvys5@gmail.com>
2022-03-03 10:43:58 +0100

    Añadidos scripts para h2

---
+++ BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/controller/MyDataControllerTest.java
@@ -0,0 +1,58 @@
+package com.businessassistantbcn.mydata.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+import com.businessassistantbcn.mydata.service.UserService;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = MydataController.class)
+class MyDataControllerTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+	
+	@MockBean
+	private UserService userService;	
+	
+	private final String
+	CONTROLLER_BASE_URL = "/businessassistantbcn/api/v1/mydata",
+	RES0 = "$.results[0].";
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("Test response Hello")
+	void test() {
+		final String URI_TEST = "/test";
+		webTestClient.get()
+					.uri(CONTROLLER_BASE_URL + URI_TEST)
+					.accept(MediaType.APPLICATION_JSON)
+					.exchange()
+					.expectStatus().isOk()
+					.expectBody(String.class)
+					.value(s -> s.toString(), equalTo("Hello from BusinessAssistant MyData!!!"));
+	}	
+	
+}
