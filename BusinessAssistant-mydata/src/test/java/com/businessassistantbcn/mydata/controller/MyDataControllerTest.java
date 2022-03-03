37432b2cf82f2f68ef35c389fc842f143b3f6ffa
Gustavo Martínez <gusmbox@gmail.com>
2022-03-03 16:35:08 +0100

    Refactored src/test/resources & configured testing profile

--- BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/controller/MyDataControllerTest.java
+++ BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/controller/MyDataControllerTest.java
@@ -12,15 +12,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 
 import com.businessassistantbcn.mydata.service.UserService;
 
+@Profile("test")
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = MydataController.class)
+@ActiveProfiles("test")
+
 class MyDataControllerTest {
 
 	@Autowired
