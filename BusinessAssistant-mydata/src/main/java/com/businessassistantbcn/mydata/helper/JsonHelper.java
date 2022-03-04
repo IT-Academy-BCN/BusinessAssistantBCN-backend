686b7ed3bd0616b706844e95d239332861d25d39
laura <laurabf6@gmail.com>
2022-03-04 18:29:33 +0100

    missing methods in JsonHelper

--- BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/helper/JsonHelper.java
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/helper/JsonHelper.java
@@ -1,8 +1,12 @@
 package com.businessassistantbcn.mydata.helper;
 
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.businessassistantbcn.mydata.entities.Search;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,11 +14,46 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
-public class JsonHelper {
+@Converter
+public class JsonHelper implements AttributeConverter<JsonNode, String> {
 	
 	private static final Logger log = LoggerFactory.getLogger(JsonHelper.class);
 	
 	private static ObjectMapper objectMapper = new ObjectMapper();
+	
+	 @Override
+	  public String convertToDatabaseColumn(JsonNode jsonNode)
+	  {
+	    if( jsonNode == null)
+	    {
+	      log.warn( "jsonNode input is null, returning null" );
+	      return null;
+	    }
+
+	    String jsonNodeString = jsonNode.toPrettyString();
+	    return jsonNodeString;
+	  }
+	 
+	 @Override
+	  public JsonNode convertToEntityAttribute(String jsonNodeString) {
+
+	    if ( StringUtils.isEmpty(jsonNodeString) )
+	    {
+	      log.warn( "jsonNodeString input is empty, returning null" );
+	      return null;
+	    }
+
+	    ObjectMapper mapper = new ObjectMapper();
+	    try
+	    {
+	      return mapper.readTree( jsonNodeString );
+	    }
+	    catch( JsonProcessingException e )
+	    {
+	      log.error( "Error parsing jsonNodeString", e );
+	    }
+	    return null;
+	  }
 
 	public static Search jsonToEntity(String jsonSearch) {
 		Search search = new Search();
