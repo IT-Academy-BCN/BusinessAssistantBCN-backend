2bbeb3f22712a846b4cb8bdc1e9f43a1e1df9ad3
Jonatan Vicente <it@jonatanvicente.com>
2022-03-04 15:17:39 +0100

    UNSTABLE- unable to register Opendata micro

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
@@ -3,6 +3,7 @@ package com.businessassistantbcn.opendata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
