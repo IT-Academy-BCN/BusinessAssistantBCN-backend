3839c8623c0996143d5df56af1c11c04ac8feba4
Jonatan Vicente <it@jonatanvicente.com>
2022-03-07 10:14:14 +0100

    Testing out of consul network

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/App.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class App {
 
     public static void main(String[] args) {
