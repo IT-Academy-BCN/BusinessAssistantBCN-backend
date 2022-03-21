d6b3afc4075e81c0651eef67201804b2198eb5b7
Joel Cabatay <ascii274@gmail.com>
2022-03-21 13:43:38 +0100

    Update dcocument/User

--- BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/document/User.java
+++ BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/document/User.java
@@ -2,12 +2,13 @@ package com.businessassistantbcn.usermanagement.document;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import javax.persistence.Id;
 import javax.persistence.Transient;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,7 +27,8 @@ public class User {
     private String password;
     @Field(name="role")
     private Role role;
+    private List<Role> roles;
 
-
+    public User() {}
 
 }
