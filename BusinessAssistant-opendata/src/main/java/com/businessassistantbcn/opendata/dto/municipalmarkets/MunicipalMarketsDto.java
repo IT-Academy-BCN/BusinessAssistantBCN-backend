766eeb3ac779294b4bb437d0083e4b0d18e2384c
laura <laurabf6@gmail.com>
2022-04-21 12:21:07 +0200

    location inside address in MunicipalMarkets

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/municipalmarkets/MunicipalMarketsDto.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/dto/municipalmarkets/MunicipalMarketsDto.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 @Component
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({"name","web","email","phone","location","addresses","activity"})
+@JsonPropertyOrder({"name","web","email","phone","addresses","activity"})
 public class MunicipalMarketsDto {
 
     @JsonIgnore
