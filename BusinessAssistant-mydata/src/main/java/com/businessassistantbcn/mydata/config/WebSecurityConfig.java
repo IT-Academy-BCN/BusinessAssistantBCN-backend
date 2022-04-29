23b741202c733f05fa6d08526c4668dc6025dbd4
Jonatan Vicente <it@jonatanvicente.com>
2022-04-29 10:26:54 +0200

    Adding endpoints security

--- BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/config/WebSecurityConfig.java
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/config/WebSecurityConfig.java
@@ -4,48 +4,44 @@ import com.businessassistantbcn.mydata.config.PropertiesConfig;
 import com.businessassistantbcn.mydata.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
-/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;*/
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/*@Configuration
-@Profile("prod")*/
-public class WebSecurityConfig{
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//} extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PropertiesConfig config;
 
-/*	@Autowired
-	private PropertiesConfig config;
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
 
-*//*	@Autowired
-	private JwtAuthenticationFilter jwtFilter;*//*
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.authorizeRequests().anyRequest().authenticated();
+        http.exceptionHandling().authenticationEntryPoint(
+                new BasicAuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+                            throws IOException {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        response.getWriter().print(config.getErr());
+                    }
+                }
+        );
+    }
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-*//*		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);*//*
-		http.authorizeRequests().anyRequest().authenticated();
-*//*		http.exceptionHandling().authenticationEntryPoint(
-			new BasicAuthenticationEntryPoint() {
-				@Override
-				public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-					throws IOException {
-					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-					response.getWriter().print(config.getErr());
-				}
-			}
-		);*//*
-	}*/
-	
 }
--- BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/security/JwtAuthenticationFilter.java
+++ BusinessAssistant-mydata/src/main/java/com/businessassistantbcn/mydata/security/JwtAuthenticationFilter.java
@@ -4,10 +4,9 @@ import com.businessassistantbcn.mydata.config.PropertiesConfig;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Autowired;
-/*import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;*/
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,55 +19,53 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class JwtAuthenticationFilter{
-	/*extends
-} OncePerRequestFilter {
-	
-	@Autowired
-	private PropertiesConfig config;
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final String TOKEN_PREFIX = "Bearer ";
-	
-	@Override
-	protected void doFilterInternal(
-		HttpServletRequest request,
-		HttpServletResponse response,
-		FilterChain filterChain)
-	throws IOException, ServletException {
-		try {
-			String authorizationHeader = request.getHeader(config.getHeaderString());
-			if(authorizationHeaderIsValid(authorizationHeader)) {
-				Claims claims = validateToken(request);
-				setUpSpringAuthentication(claims);
-			}
-			filterChain.doFilter(request, response);
-		} catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-		}
-	}
-	
-	private boolean authorizationHeaderIsValid(String authorizationHeader) {
-		return authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX);
-	}
-	
-	private Claims validateToken(HttpServletRequest request) {
-		String jwtToken = request.getHeader(config.getHeaderString()).replace(TOKEN_PREFIX, "").trim();
-		return Jwts.parserBuilder()
-			.setSigningKey(config.getSecret().getBytes())
-			.build()
-			.parseClaimsJws(jwtToken)
-			.getBody();
-	}
-	
-	private void setUpSpringAuthentication(Claims claims) {
-		@SuppressWarnings("unchecked")
-		List<String> authorities = (List<String>)claims.get(config.getAuthoritiesClaim());
-		
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-				claims.getSubject(), null,
-				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
-		
-		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-	}
-*/
+    @Autowired
+    private PropertiesConfig config;
+
+    private final String TOKEN_PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws IOException, ServletException {
+        try {
+            String authorizationHeader = request.getHeader(config.getHeaderString());
+            if(authorizationHeaderIsValid(authorizationHeader)) {
+                Claims claims = validateToken(request);
+                setUpSpringAuthentication(claims);
+            }
+            filterChain.doFilter(request, response);
+        } catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    private boolean authorizationHeaderIsValid(String authorizationHeader) {
+        return authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX);
+    }
+
+    private Claims validateToken(HttpServletRequest request) {
+        String jwtToken = request.getHeader(config.getHeaderString()).replace(TOKEN_PREFIX, "").trim();
+        return Jwts.parserBuilder()
+                .setSigningKey(config.getSecret().getBytes())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private void setUpSpringAuthentication(Claims claims) {
+        @SuppressWarnings("unchecked")
+        List<String> authorities = (List<String>)claims.get(config.getAuthoritiesClaim());
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                claims.getSubject(), null,
+                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
 }
