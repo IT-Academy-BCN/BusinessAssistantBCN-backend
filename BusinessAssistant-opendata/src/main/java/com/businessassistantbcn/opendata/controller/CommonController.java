4a40f84b43c7a69612d86a10ecf1047c1a2507af
Jonatan Vicente <it@jonatanvicente.com>
2022-04-30 09:25:38 +0200

    Removing Swagger old versions

--- BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/CommonController.java
+++ BusinessAssistant-opendata/src/main/java/com/businessassistantbcn/opendata/controller/CommonController.java
@@ -1,9 +1,6 @@
 package com.businessassistantbcn.opendata.controller;
 
 import com.businessassistantbcn.opendata.service.config.DataConfigService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +15,6 @@ public class CommonController {
     DataConfigService bcnZonesService;
 
     @GetMapping("/bcn-zones")
-    @ApiOperation("Get bcn Zones")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 404, message = "Not Found")
-    })
     public Mono<?> bcnZones(){
         return bcnZonesService.getBcnZones();
     }
