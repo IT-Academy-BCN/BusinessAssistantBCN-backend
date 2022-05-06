39ebbbf2c34c26705213a19152cda6daa7f3eb77
laura <laurabf6@gmail.com>
2022-05-06 17:01:08 +0200

    repositoryTest context problems fixed

--- BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/repository/UserSearchesRepositoryTest.java
+++ BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/repository/UserSearchesRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.businessassistantbcn.mydata.repository;
 
-import com.businessassistantbcn.mydata.config.DBTestConfiguration;
 import com.businessassistantbcn.mydata.entity.UserSearch;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -10,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +25,7 @@ import java.util.HashMap;
 
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { DBTestConfiguration.class })
+//@ContextConfiguration(classes = { DBTestConfiguration.class })
 @DataJpaTest
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
 //CAUTION: For uses with real database
