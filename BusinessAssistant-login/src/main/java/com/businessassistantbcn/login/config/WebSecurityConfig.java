8f7e6fbdec9e7d6ca0b479083b73a37af9c94fad
magaangel <93588541+magaangel@users.noreply.github.com>
2022-05-11 17:18:35 +0200

    Adicion de Swagger a login

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.POST, config.getSignUpUrl()).permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
 		http.exceptionHandling().authenticationEntryPoint(new BasicAuthenticationEntryPoint() {
-			
+
 			@Override
 			public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
 				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -54,6 +55,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			}
 			
 		});
+	}
+
+	@Override
+	public void configure(WebSecurity web){
+		web.ignoring()
+				.antMatchers("/swagger-ui/**",
+						"/swagger-ui-custom.html",
+						"/api-docs/**")
+				.anyRequest();
 	}
 	
 }
