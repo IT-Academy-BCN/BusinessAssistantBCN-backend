10af4aff93f2bb6558866a4b0366bf8615e6d2e9
laura <laurabf6@gmail.com>
2022-05-25 16:08:27 +0200

    remove unnecessary code

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/HttpSecurityConfig.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/config/HttpSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.businessassistantbcn.login.config;
 
 import com.businessassistantbcn.login.security.JwtAuthenticationFilter;
-import com.businessassistantbcn.login.service.LoginService;
 
 import java.io.IOException;
 
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -30,14 +28,6 @@ public class HttpSecurityConfig {
 	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 	@EnableWebSecurity
 	public static class DisableSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Autowired
-		private LoginService provider;
-
-		@Override
-		public void configure(AuthenticationManagerBuilder builder) throws Exception {
-			builder.authenticationProvider(provider);
-		}
-
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests().anyRequest().permitAll();
@@ -48,6 +38,7 @@ public class HttpSecurityConfig {
 			web.ignoring().antMatchers("/swagger-ui/**", "/swagger-ui-custom.html", "/api-docs/**").anyRequest();
 		}
 	}
+	
 	@Profile("pro")
 	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 	@EnableWebSecurity
