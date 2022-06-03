48bbd6bc7a852935c45ba86a61564303795ba7df
Jordi Borràs i Vivó <jordibv@gmail.com>
2022-06-03 11:16:41 +0200

    assertions

--- BusinessAssistant-usermanagement/src/test/java/com/businessassistantbcn/usermanagement/repository/DBConfigurationTest.java
+++ BusinessAssistant-usermanagement/src/test/java/com/businessassistantbcn/usermanagement/repository/DBConfigurationTest.java
@@ -5,17 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataMongoTest
 @TestPropertySource(locations = "classpath:persistence-test.properties")
-public class DBConfigurationTest {
+class DBConfigurationTest {
 
     @Autowired
     private Environment env;
 
     @Test
-    public void smokeTest() {
-        System.out.println("activeProfiles:"+env.getProperty("spring.data.mongodb.port"));
+    void smokeTest() {
+        String port = env.getProperty("spring.data.mongodb.port");
+        String database = env.getProperty("spring.data.mongodb.database");
+        String version = env.getProperty("spring.mongodb.embedded.version");
+        assertEquals(27018, Integer.parseInt(port));
+        assertEquals("test", database);
+        assertEquals("3.2.2", version);
     }
 
 }
