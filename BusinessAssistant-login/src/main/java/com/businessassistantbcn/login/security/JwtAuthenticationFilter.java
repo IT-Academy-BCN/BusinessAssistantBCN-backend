96ede047bbeba3a2f069bb8a01244cc8c25cce67
quiquelowbeat <kikeebcn@gmail.com>
2022-06-17 10:47:18 +0200

    Updated JwtAuthenticationFilter

--- BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
+++ BusinessAssistant-login/src/main/java/com/businessassistantbcn/login/security/JwtAuthenticationFilter.java
@@ -1,7 +1,5 @@
 package com.businessassistantbcn.login.security;
 
-import com.businessassistantbcn.login.config.SecurityConfig;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -27,53 +25,55 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.businessassistantbcn.login.config.ApiSecurityConfig;
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-	
+
 	@Autowired
-	private SecurityConfig config;
-	
+	private ApiSecurityConfig config;
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
-			HttpServletResponse response,
-			FilterChain filterChain) throws IOException, ServletException { try {
-		
-		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-		
-		if(authorizationHeaderIsValid(authorizationHeader)) {
-			Claims claims = validateToken(request);
-			if(claims.getExpiration() != null && claims.get(config.getAuthoritiesClaim()) != null)
+									HttpServletResponse response,
+									FilterChain filterChain) throws IOException, ServletException {
+		try {
+
+			String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+			if(authorizationHeaderIsValid(authorizationHeader)) {
+				Claims claims = validateToken(request);
 				setUpSpringAuthentication(claims);
+			}
+			filterChain.doFilter(request, response);
+		} catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 		}
-		
-		filterChain.doFilter(request, response);
-	} catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-	} }
-	
+	}
+
 	private boolean authorizationHeaderIsValid(String authorizationHeader) {
 		return authorizationHeader != null && authorizationHeader.startsWith(config.getTokenPrefix());
 	}
-	
+
 	private Claims validateToken(HttpServletRequest request) {
 		String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION).replace(config.getTokenPrefix(), "");
-		
+
 		return Jwts.parserBuilder()
 				.setSigningKey(config.getSecret().getBytes())
 				.build()
 				.parseClaimsJws(jwtToken) // Incluye la comprobación temporal para claim 'exp'
 				.getBody();
 	}
-	
+
 	private void setUpSpringAuthentication(Claims claims) {
 		@SuppressWarnings("unchecked")
 		List<String> authorities = (List<String>)claims.get(config.getAuthoritiesClaim());
-		
+
 		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
 				claims.getSubject(), null,
 				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
-		
+
 		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 	}
-	
+
 }
