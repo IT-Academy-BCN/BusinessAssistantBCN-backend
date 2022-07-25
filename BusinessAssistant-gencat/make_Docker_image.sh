0d5b689bd7ac677729773046f23654292ed368c5
Jonatan Vicente <it@jonatanvicente.com>
2022-07-25 12:46:06 +0200

    Adding consul & Docker & docker compose configs

--- BusinessAssistant-gencat/make_Docker_image.sh
+++ BusinessAssistant-gencat/make_Docker_image.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
 cd ..
+# CAUTION: avoid tests, only for development purposes
 ./gradlew :BusinessAssistant-gencat:build -x test
+#./gradlew :BusinessAssistant-opendata:build
 
 cd BusinessAssistant-gencat
 docker build -t=babcn:gencat-v1.0-SNAPSHOT .
-
-cd ..
-docker-compose up businessassistantbcn-gencat
