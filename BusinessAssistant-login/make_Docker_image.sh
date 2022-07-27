669845bbeb55a00d50aa0da1b7a2b233edb354bd
Jonatan Vicente <it@jonatanvicente.com>
2022-07-27 19:36:40 +0200

    Adding config: login service & usermanagement; Mongodb registry

---
+++ BusinessAssistant-login/make_Docker_image.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ..
+# CAUTION: avoid tests, only for development purposes
+./gradlew :BusinessAssistant-login:build -x test
+#./gradlew :BusinessAssistant-opendata:build
+
+cd BusinessAssistant-login
+docker build -t=babcn:login-v1.0-SNAPSHOT .
