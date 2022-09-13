8e2a32ff5449e85cc06fe295f75b99ea7921af36
Jonatan Vicente <it@jonatanvicente.com>
2022-09-13 12:57:05 +0200

    Fixed Docker entrypoint relative path

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+## Initial requisites:
+##  - Verify Docker is running on system (sudo systemctl start docker)
+##  - Uncomment lines at build.gradle that contains spring-cloud dependencies (3 lines)
+
+## Some variables
 DELAY_START=20
 now="$(date +'%d-%m-%Y %H:%M:%S:%3N')"
 
@@ -16,6 +21,14 @@ sleep ${DELAY_START}s
   echo ""
   echo ""
 
+echo " Starting Database Mongo in daemon mode"
+docker compose -f consul/consul-compose.yml up businessassistantbcn-mongodb -d
+sleep ${DELAY_START}s
+
+echo " Starting businessassistantbcn-usermanagement in daemon mode"
+docker compose -f consul/consul-compose.yml up businessassistantbcn-usermanagement
+sleep ${DELAY_START}s
+
 echo " Starting businessassistantbcn-opendata in daemon mode"
 docker compose -f consul/consul-compose.yml up -d businessassistantbcn-opendata
 sleep ${DELAY_START}s
@@ -23,6 +36,11 @@ sleep ${DELAY_START}s
 echo " Starting businessassistantbcn-gencat in daemon mode"
 docker compose -f consul/consul-compose.yml up -d businessassistantbcn-gencat
 sleep ${DELAY_START}s
+
+echo " Starting businessassistantbcn-login in daemon mode"
+docker compose -f consul/consul-compose.yml up businessassistantbcn-login
+sleep ${DELAY_START}s
+
 echo ""
 echo " ================ THE END ;) ==============="
 
