50581b81ef7438c454c428ad2f37f742daf1050b
LuisEGamez <luisgamezdvp@gmail.com>
2022-09-28 16:54:38 +0200

    adding controller /ccae test

--- BusinessAssistant-gencat/src/test/java/com/businessassistantbcn/gencat/controller/GencatControllerTest.java
+++ BusinessAssistant-gencat/src/test/java/com/businessassistantbcn/gencat/controller/GencatControllerTest.java
@@ -1,6 +1,9 @@
 package com.businessassistantbcn.gencat.controller;
 
 import com.businessassistantbcn.gencat.config.PropertiesConfig;
+import com.businessassistantbcn.gencat.dto.GenericResultDto;
+import com.businessassistantbcn.gencat.dto.io.CcaeDto;
+import com.businessassistantbcn.gencat.dto.io.CodeInfoDto;
 import com.businessassistantbcn.gencat.proxy.HttpProxy;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +24,10 @@ import org.springframework.util.ResourceUtils;
 import reactor.core.publisher.Mono;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
@@ -31,7 +37,6 @@ import static org.mockito.Mockito.*;
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = GencatController.class, excludeAutoConfiguration = {ReactiveSecurityAutoConfiguration.class})
 class GencatControllerTest {
-
     @Autowired
     private WebTestClient webTestClient;
 
@@ -47,18 +52,45 @@ class GencatControllerTest {
     private CcaeService ccaeService;
 
     private final String CONTROLLER_BASE_URL = "/businessassistantbcn/api/v1/gencat";
+    private static final String RES0 = "$.results[0].";
     private static String urlEconomicActivitiesString;
     private static final String JSON_FILENAME = "/get5-imi7.json";
     private static ObjectMapper mapper;
     private static Object[] economicActivities;
 
+    private CcaeDto[] responseDto;
 
+    @BeforeEach
+    void setUp() {
+        responseDto = new CcaeDto[2];
+
+        CcaeDto ccaeDto1 = new CcaeDto();
+        String id1 = "00000000-0000-0000-D7DC-CC770365D8FF";
+        String type1 = "Secció";
+        String idCodeInfo1 = "A";
+        String codeDescription1 = "Agricultura, ramaderia, silvicultura i pesca";
+        CodeInfoDto codeInfoDto1 = new CodeInfoDto(idCodeInfo1, codeDescription1);
+        ccaeDto1.setId(id1);
+        ccaeDto1.setType(type1);
+        ccaeDto1.setCode(codeInfoDto1);
+        responseDto[0] = ccaeDto1;
+
+        CcaeDto ccaeDto2 = new CcaeDto();
+        String id2 = "00000000-0000-0000-2335-839767DDAEAB";
+        String type2 = "Divisió";
+        String idCodeInfo2 = "01";
+        String codeDescription2 = "Agricultura, ramaderia, caça i activitats dels serveis que s'hi relacionen";
+        CodeInfoDto codeInfoDto2 = new CodeInfoDto(idCodeInfo2, codeDescription2);
+        ccaeDto2.setId(id2);
+        ccaeDto2.setType(type2);
+        ccaeDto2.setCode(codeInfoDto2);
+        responseDto[1] = ccaeDto2;
+    }
 
     @Test
     void testHello() {
 
         final String URI_TEST = "/test";
-
         webTestClient.get()
                 .uri(CONTROLLER_BASE_URL + URI_TEST)
                 .accept(MediaType.APPLICATION_JSON)
@@ -68,15 +100,31 @@ class GencatControllerTest {
                 .value(s -> s.toString(), equalTo("Hello from GenCat Controller!!!"));
     }
 
+
+
     @Test
-    void getEconomicActivities() {
+    void getEconomicActivities() throws MalformedURLException {
         final String URI_TEST = "/ccae";
+        when(ccaeService.getPage(0,-1)).thenReturn(Mono.just(getGenericResultDto()));
         webTestClient.get()
                 .uri(CONTROLLER_BASE_URL + URI_TEST)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isEqualTo(HttpStatus.OK)
-                .expectBody();
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.count").isEqualTo(2)
+                .jsonPath("$.offset").isEqualTo(0)
+                .jsonPath("$.limit").isEqualTo(-1)
+                .jsonPath(RES0 + "id").isEqualTo("00000000-0000-0000-D7DC-CC770365D8FF")
+                .jsonPath(RES0 + "type").isEqualTo("Secció")
+                .consumeWith(System.out::println);
+    }
+
+    private GenericResultDto<CcaeDto> getGenericResultDto() {
+        GenericResultDto<CcaeDto> genericResultDto = new GenericResultDto<>();
+        genericResultDto.setInfo(0, -1, responseDto.length, responseDto);
+        return genericResultDto;
     }
 
     //Una vez implementado correctamente el método, el test se debe adecuar
