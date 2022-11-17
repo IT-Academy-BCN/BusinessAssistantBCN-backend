2f466bf5a2194bb70a3d8a8d82b836a9f1a09ce3
Jonatan Vicente <it@jonatanvicente.com>
2022-11-17 12:56:02 +0100

    Externalizing pre & pro values

--- init.sh
+++ init.sh
@@ -16,37 +16,37 @@ echo "Date: "${now}
   echo " ======================================================"
   echo ""
   echo " Starting consul-server1, consul-server2 and consul-server3 in daemon mode"
-docker compose -f consul/consul-compose.yml up -d consul-server1 consul-server2 consul-server3
+docker compose -f consul/docker-compose.yml up -d consul-server1 consul-server2 consul-server3
 sleep ${DELAY_START}s
   echo ""
   echo ""
 
 echo " Starting Database Mongo in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-mongodb -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-mongodb -d
 sleep ${DELAY_START}s
 
 echo " Starting Database Mysql in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-mysql -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-mysql -d
 sleep ${DELAY_START}s
 
 echo " Starting businessassistantbcn-usermanagement in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-usermanagement -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-usermanagement -d
 sleep ${DELAY_START}s
 
 echo " Starting businessassistantbcn-mydata in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-mydata -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-mydata -d
 sleep ${DELAY_START}s
 
 echo " Starting businessassistantbcn-opendata in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-opendata -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-opendata -d
 sleep ${DELAY_START}s
 
 echo " Starting businessassistantbcn-gencat in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-gencat -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-gencat -d
 sleep ${DELAY_START}s
 
 echo " Starting businessassistantbcn-login in daemon mode"
-docker compose -f consul/consul-compose.yml up businessassistantbcn-login -d
+docker compose -f consul/docker-compose.yml up businessassistantbcn-login -d
 sleep ${DELAY_START}s
 
 echo ""
