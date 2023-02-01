e7579c4d7d35ffb9f4929e1a0cf6c6dcd6c007f7
Alex Fernandez <alexfernandez@Sergios-MacBook-Pro.local>
2023-02-01 09:35:41 +0100

    add LimitDbErrorDto to capture message

---
+++ BusinessAssistant-usermanagement/src/main/java/com/businessassistantbcn/usermanagement/dto/LimitDbErrorDto.java
@@ -0,0 +1,14 @@
+package com.businessassistantbcn.usermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter
+@AllArgsConstructor
+@ToString
+public class LimitDbErrorDto {
+
+    private String message;
+}
