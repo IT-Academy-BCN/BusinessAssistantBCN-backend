a9aa5ea5147a62009fd0aada1250ad58db3ea2c0
Alex Fernandez <alexfernandez@Sergios-MacBook-Pro.local>
2023-02-02 11:07:58 +0100

    generate id for dummy test

--- BusinessAssistant-usermanagement/src/test/java/com/businessassistantbcn/usermanagement/helper/DtoHelperTest.java
+++ BusinessAssistant-usermanagement/src/test/java/com/businessassistantbcn/usermanagement/helper/DtoHelperTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.businessassistantbcn.usermanagement.dto.UserEmailDto;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -46,8 +47,8 @@ public class DtoHelperTest {
 
         roles.add(Role.ADMIN);
         roles.add(Role.USER);
-        user = new User("7e10fe51-772e-441f-874d-1c03dee79ad9",
-                "user@Dto.es", "1234", roles);
+        user = new User(new ObjectId("63d9666bbdf0196d2c766fa0"),"7e10fe51-772e-441f-874d-1c03dee79ad9",
+                "user@Dto.es", "1234", roles, System.currentTimeMillis());
 
         userDto = DtoHelper.convertToDto(user);
 
@@ -77,8 +78,8 @@ public class DtoHelperTest {
         rolesString.add("ADMIN");
         UserDto userDto = new UserDto("7e10fe51-772e-441f-874d-1c03dee79ad9",
                 "user@Dto.es", rolesString);
-        User user = new User("7e10fe51-772e-441f-874d-1c03dee79ad9",
-                "user@Dto.es", "1234", null);
+        User user = new User(new ObjectId("63d9666bbdf0196d2c766fa0"),"7e10fe51-772e-441f-874d-1c03dee79ad9",
+                "user@Dto.es", "1234", null, System.currentTimeMillis());
 
         roles = new ArrayList<Role>();
         roles.add(Role.USER);
@@ -93,8 +94,8 @@ public class DtoHelperTest {
         roles.add(Role.USER);
         roles.add(Role.ADMIN);
 
-        User user = new User("7e10fe51-772e-441f-874d-1c03dee79ad9",
-                "user@Dto.es", "1234", roles);
+        User user = new User(new ObjectId("63d9666bbdf0196d2c766fa0"),"7e10fe51-772e-441f-874d-1c03dee79ad9",
+                "user@Dto.es", "1234", roles, System.currentTimeMillis());
 
 
         rolesString = new ArrayList<String>();
